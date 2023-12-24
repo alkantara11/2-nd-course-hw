9a5aac14c78d5a84948f690009a062c3a32d37f1
alkantara11 <alkantara15@yandex.ru>
2023-12-24 17:29:40 +0300

    9/10 hw2

---
+++ skript.js
@@ -0,0 +1,35 @@
+let aa = alert ('20');
+let year = alert ('2007');
+let name = alert ('Брендан Айк');
+let one = 10;
+let two = 2;
+alert (one - two);
+alert (one + two);
+alert (one * two);
+alert (one / two);
+let st = 2;
+let result = st ** 5;
+alert (result);
+let a = 9;
+let b = 2;
+result = a % b;
+alert(result);
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+let age = Number(prompt('сколько вам лет?'));
+alert(age);
+const user = {
+    name: Вась,
+    age: 11,
+    isAdmin: false
+}
+user['city of residence'] = Mishkin;
+delete user['city of residence'];
+let info = prompt('Какую информацию хотите узнать о пользователе?');
+
