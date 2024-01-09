a59e4e91db6c9ba30d5b3d63d9fce98838f48371
alkantara11 <alkantara15@yandex.ru>
2024-01-09 21:20:35 +0300

    сделал 4 задания из 6

--- script_hw4.js
+++ script_hw4.js
@@ -1,1 +1,26 @@
-alert ('hi')
+let a = 0;
+while (a < 2) {
+    console.log ("Привет!");
+    a++;
+} 
+let i = 1;
+while (i <= 5) {
+    console.log(i);
+    i++; 
+}
+let ii = 7;
+while (ii <= 22) {
+    console.log(i);
+    ii++; 
+}
+const obj = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400',
+}
+for (let key in obj) {
+    console.log (`${key} — зарплата ${obj[key]}`);
+}
+for (n = 1000; n > 50; n / 2) {
+    console.log (n)
+} 
